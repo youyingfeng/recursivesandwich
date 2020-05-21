97ab423eae375e5b4a2aff2de7ffcbda0dcf3f35
Yingfeng <42577406+youyingfeng@users.noreply.github.com>
2020-05-21 19:20:38 +0800

    Moved all functions related to maps to a new file

--- main.py
+++ main.py
@@ -2,21 +2,10 @@ import pygame as pg
 from modules.block import Block
 from modules.camera import Camera
 from modules.player import Player
+from modules.map import Map
 
 
 # ---------- FUNCTION DEFINITIONS ---------- #
-
-# Loads the game map as a 2D array from a .txt file
-def load_map(path: str):
-    file = open(path, 'r')
-    data = file.read()
-    file.close()
-    data = data.split('\n')
-    game_map = []
-    for row in data:
-        game_map.append(list(row))
-    return game_map
-
 
 # Moves player according to user's keyboard inputs
 def handle_input(player, terrain):
@@ -26,25 +15,11 @@ def handle_input(player, terrain):
                 current_keys[pg.K_SPACE],
                 terrain)
 
-
-# Reads a game map and adds Blocks accordingly to the Sprite Group
-def add_blocks_to_group(group: pg.sprite.Group, map):
-    for y in range(len(map)):
-        for x in range(len(map[0])):
-            if map[y][x] == '1':
-                group.add(Block(grass_img, x * Block.BLOCK_SIZE, y * Block.BLOCK_SIZE))
-            elif map[y][x] == '2':
-                group.add(Block(dirt_img, x * Block.BLOCK_SIZE, y * Block.BLOCK_SIZE))
-
-
 # ---------- GLOBAL VARIABLES ---------- #
 
 # Textures
-grass_img = pg.image.load('assets/textures/grass.png')
-dirt_img = pg.image.load('assets/textures/dirt.png')
-
-# Game Map (as 2D array)
-game_map = load_map('assets/maps/map2.txt')
+# grass_img = pg.image.load('assets/textures/grass.png')
+# dirt_img = pg.image.load('assets/textures/dirt.png')
 
 WINDOW_SIZE = (800, 600)
 SURFACE_SIZE = (400, 300)
@@ -56,15 +31,17 @@ pg.init()                                                   # Initialize pygame
 window = pg.display.set_mode(WINDOW_SIZE)                   # Initialize game window
 pg.display.set_caption("The Tower", "The Tower")            # Set window caption
 game_display = pg.Surface(SURFACE_SIZE)                     # Initialize surface for drawing
-map_rect = pg.Rect(0, 0, len(game_map[0]), len(game_map))   # Initialize Rect representing entire map
+
+game_map = Map('assets/maps/map2.txt')                      # Game Map (as 2D array)
+map_rect = pg.Rect((0, 0), game_map.dimensions)             # Initialize Rect representing entire map
+terrain_group = game_map.terrain_group                      # Initialize terrain sprite group
+
 camera = Camera(SURFACE_SIZE)                               # Initialize camera
+
 clock = pg.time.Clock()                                     # Initialize clock
 
 player = Player()                                           # Initialize player
 player_sprite_group = pg.sprite.GroupSingle(player)         # Initialize player sprite group
-
-terrain_group = pg.sprite.Group()                           # Initialize terrain sprite group
-add_blocks_to_group(terrain_group, game_map)
 
 all_sprites_group = pg.sprite.Group()                       # Initialize all sprites group
 all_sprites_group.add(player)
